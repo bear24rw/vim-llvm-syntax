8b47ecbffc26d93b2a3ee5073a26384358ee27a5
Max Thrun <bear24rw@gmail.com>
2014-03-27 17:59:31 -0400

    init commit

---
+++ update.sh
@@ -0,0 +1,2 @@
+wget -O syntax/llvm.vim https://llvm.org/viewvc/llvm-project/llvm/trunk/utils/vim/llvm.vim?view=co
+wget -O syntax/tablegen.vim https://llvm.org/viewvc/llvm-project/llvm/trunk/utils/vim/tablegen.vim?view=co
